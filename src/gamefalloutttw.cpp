f780d907befe9f1b9ac69ec62d84361c4b6618cc
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-16 23:28:17 +0200

    Add ModDataChecker feeature (generic).

--- src/gamefalloutttw.cpp
+++ src/gamefalloutttw.cpp
@@ -9,6 +9,7 @@
 #include "pluginsetting.h"
 #include "versioninfo.h"
 #include <gamebryolocalsavegames.h>
+#include <gamebryomoddatachecker.h>
 #include <gamebryogameplugins.h>
 #include <gamebryounmanagedmods.h>
 
@@ -40,6 +41,7 @@ bool GameFalloutTTW::init(IOrganizer *moInfo)
   registerFeature<BSAInvalidation>(new FalloutTTWBSAInvalidation(feature<DataArchives>(), this));
   registerFeature<SaveGameInfo>(new FalloutTTWSaveGameInfo(this));
   registerFeature<LocalSavegames>(new GamebryoLocalSavegames(myGamesPath(), "fallout.ini"));
+  registerFeature<ModDataChecker>(new GamebryoModDataChecker(this));
   registerFeature<GamePlugins>(new GamebryoGamePlugins(moInfo));
   registerFeature<UnmanagedMods>(new GamebryoUnmangedMods(this));
   return true;
