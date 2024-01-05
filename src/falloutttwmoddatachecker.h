022172f5b920e061a567890ab9e223f1a19afa28
Liderate <Liderate@users.noreply.github.com>
2024-01-05 02:12:51 -0500

    KEYWORDS and BaseObjectSwapper valid folders.

--- src/falloutttwmoddatachecker.h
+++ src/falloutttwmoddatachecker.h
@@ -9,21 +9,24 @@ public:
   using GamebryoModDataChecker::GamebryoModDataChecker;
 
 protected:
-  virtual const FileNameSet& possibleFolderNames() const override {
-    static FileNameSet result{
-      "fonts", "interface", "menus", "meshes", "music", "scripts", "shaders",
-      "sound", "strings", "textures", "trees", "video", "facegen", "materials",
-      "nvse", "distantlod", "asi", "Tools", "MCM", "distantland", "mits",
-      "dllplugins", "CalienteTools", "shadersfx", "config"
-    };
+  virtual const FileNameSet& possibleFolderNames() const override
+  {
+    static FileNameSet result{"fonts",      "interface",     "menus",
+                              "meshes",     "music",         "scripts",
+                              "shaders",    "sound",         "strings",
+                              "textures",   "trees",         "video",
+                              "facegen",    "materials",     "nvse",
+                              "distantlod", "asi",           "Tools",
+                              "MCM",        "distantland",   "mits",
+                              "dllplugins", "CalienteTools", "shadersfx",
+                              "config",     "KEYWORDS",      "BaseObjectSwapper"};
     return result;
   }
-  virtual const FileNameSet& possibleFileExtensions() const override {
-    static FileNameSet result{
-      "esp", "esm", "bsa", "modgroups", "ini"
-    };
+  virtual const FileNameSet& possibleFileExtensions() const override
+  {
+    static FileNameSet result{"esp", "esm", "bsa", "modgroups", "ini"};
     return result;
   }
 };
 
-#endif // FALLOUTTTW_MODATACHECKER_H
+#endif  // FALLOUTTTW_MODATACHECKER_H
